fb38f49045a8c70e2ce36a6c7a75e60ce12f9d4e
Heshamazab <60970771+Heshamazab1999@users.noreply.github.com>
2023-07-29 01:09:19 +0200

    finish

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import Firebase
+import FirebaseMessaging
 import GoogleMaps
 import flutter_downloader
 import FBSDKCoreKit
@@ -8,6 +9,7 @@ import FBSDKLoginKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -18,7 +20,25 @@ import FBSDKLoginKit
     FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+   override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken : Data){
+          Messaging.messaging().apnsToken=deviceToken
+          print("Token : \(deviceToken)")
+              try! setExcludeFromiCloudBackup(isExcluded: true)
+          super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+
+      }
+
+       private func setExcludeFromiCloudBackup(isExcluded: Bool) throws {
+              var fileOrDirectoryURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+              var values = URLResourceValues()
+              values.isExcludedFromBackup = isExcluded
+              try fileOrDirectoryURL.setResourceValues(values)
+          }
 }
+
+
+
+
 private func registerPlugins(registry: FlutterPluginRegistry) {
     if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
        FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
